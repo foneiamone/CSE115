64a82cbfb306abd07d5de8e3b3e05382fba0bfb3
foneiamone <foneiamone@gmail.com>
2022-05-15 15:10:43 +0600

    gh0$t

---
+++ Practice/hanka.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+void main()
+{
+    system("cls");
+    printf("H_");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A_");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N_");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K_");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A_");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A");
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A_");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A \nR|");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A \nR O|");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A \nR O B|");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A \nR O B O|");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A \nR O B O T|");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A \nR O B O T I|");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A \nR O B O T I C|");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A \nR O B O T I C S|");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A \nR O B O T I C S");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A \nR O B O T I C S|");
+    fflush(stdout);
+
+    sleep(1);
+    system("cls");
+    printf("H A N K A \nR O B O T I C S");
+    fflush(stdout);
+}
